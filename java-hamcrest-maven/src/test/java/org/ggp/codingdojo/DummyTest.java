9a74d09f564d272108004d5cb4bbb17c97c17b23
unknown <pasqualinog@LATITUDEE6530-SSD1.ITWAREKFT.local>
2014-12-03 17:32:55 +0100

    Adds java template with hamcrest in maven

---
+++ java-hamcrest-maven/src/test/java/org/ggp/codingdojo/DummyTest.java
@@ -0,0 +1,14 @@
+package org.ggp.codingdojo;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void unnamedTest() {
+		assertThat("fixme", equalTo("foo"));
+	}
+}
